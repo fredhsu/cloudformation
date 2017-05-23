ddbd7117d3a4ff14628d9004f6a18dcd8347c501
Fred Hsu <fredlhsu@gmail.com>
2017-05-23 15:38:29 -0700

    adding multi-region template and script

---
+++ deletemulti.sh
@@ -0,0 +1,7 @@
+#regions="us-west-2 us-east-2 eu-west-1 ap-southeast-2 ap-northeast-1"
+regions="us-west-2 eu-west-1 ap-southeast-2"
+for r in $regions; do
+    aws cloudformation delete-stack \
+        --region $r \
+        --stack-name "CT-$r" 
+done
---
+++ multiregion.sh
@@ -0,0 +1,9 @@
+#regions="us-west-2 us-east-2 eu-west-1 ap-southeast-2 ap-northeast-1"
+regions="us-west-2 eu-west-1 ap-southeast-2"
+for r in $regions; do
+    aws cloudformation create-stack \
+        --region $r \
+        --stack-name "CT-$r" \
+        --template-body file://multiregion-ec2.yaml
+done
+
