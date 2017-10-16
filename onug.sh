65dad6f6cc7b37b6ec0c89dae2554e55ea4168e6
Fred Hsu <fredlhsu@gmail.com>
2017-10-16 14:15:59 -0700

    added user data for iperf

--- onug.sh
+++ onug.sh
@@ -1,7 +1,7 @@
 #regions="us-west-2 us-east-2 eu-west-1 ap-southeast-2 ap-northeast-1"
 # regions="us-west-2 eu-west-1 ap-southeast-2"
 # regions="us-west-2"
-for i in `seq 10 15`; do
+for i in `seq 1 40`; do
     aws cloudformation create-stack \
         --region ca-central-1 \
         --stack-name "vpc-$i" \
