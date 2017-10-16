c9005d444269f979f547d48688e13e77839113e1
Fred Hsu <fredlhsu@gmail.com>
2017-10-16 10:31:03 -0700

    changed resource name tags to match vipul's docs

--- onug-clean.sh
+++ onug-clean.sh
@@ -1,7 +1,7 @@
 #regions="us-west-2 us-east-2 eu-west-1 ap-southeast-2 ap-northeast-1"
 # regions="us-west-2 eu-west-1 ap-southeast-2"
 # regions="us-west-2"
-for i in `seq 10 20`; do
+for i in `seq 10 15`; do
     aws cloudformation delete-stack \
         --region ca-central-1 \
         --profile selab \
